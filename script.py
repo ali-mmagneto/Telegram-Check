1fa2390b3e9b3bcb1babcd4d441b5ed3ebb35a17
Trelicon <88194972+trelicon@users.noreply.github.com>
2022-08-19 23:40:32 +0300

    Update script.py

--- script.py
+++ script.py
@@ -26,6 +26,6 @@ async def function(client, message, text):
        
 @app.on_message(filters.private)
 async def check(client, message):
-    await function(client, message, text)
+    await function(client, message, TEXT)
 
 app.run()
