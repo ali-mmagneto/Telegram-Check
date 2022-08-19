8d4e265a6b9cd963d01ae6c1291e610e68cbefc0
Trelicon <88194972+trelicon@users.noreply.github.com>
2022-08-19 22:29:05 +0300

    Update script.py

--- script.py
+++ script.py
@@ -10,18 +10,15 @@ app = Client("my_account", api_id=API_ID, api_hash=API_HASH, session_string=SESS
 async def function(client, message, text):
     time.sleep(6)
     try:
-       message = await message.reply(text)
-       check_text = message.reply_to_message.text
+       messages = await message.reply(text)
+       check_text = messages.reply_to_message.text
        x = re.search("Sorry", check_text)
        if x:
          pass
        else:
          await client.send_message(USERNAME, text)
-         try:
-           chat = await client.create_channel("Hayırlı olsun", "Channel Description")
-           await client.set_chat_username(chat.id, text)
-         except:
-            pass
+         chat = await client.create_channel("Hayırlı olsun", "Channel Description")
+         await client.set_chat_username(chat.id, text)
     except FloodWait as e:
        print(f"Sleep of {e.value} required by FloodWait ...")
        time.sleep(e.value)
